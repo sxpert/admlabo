5fcfe294469fb897fbcc57912df1f488dea73154
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2015-03-23 15:13:39 +0100

    add logout procedure

---
+++ admipag/AdminToolCore/views/logout_view.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import logout
+
+
+import logging
+logger=logging.getLogger('django')
+
+def logout_view (request) :
+	logger.error('logging out '+request.user.username)
+	logout (request)
+	return redirect('dashboard')
+
