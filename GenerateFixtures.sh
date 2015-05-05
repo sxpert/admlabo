54462761f23dc042fe4736ec604af10528689645
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2015-05-05 15:09:09 +0200

    Add generating fixtures (base data)
    * GenerateFixtures.sh : call dumpdata for each table
    * *.json : contains the basic data

---
+++ GenerateFixtures.sh
@@ -0,0 +1,3 @@
+./manage.py dumpdata AdminTool admtooCore.Country > admtooCore/fixtures/Country.json
+./manage.py dumpdata AdminTool admtooCore.Office > admtooCore/fixtures/Office.json
+./manage.py dumpdata AdminTool admtooCore.EmailAlert > admtooCore/fixtures/EmailAlert.json
