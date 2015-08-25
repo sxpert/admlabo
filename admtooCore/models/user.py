45ecc3096bf2773e9536235c0949e803a1b4cae2
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2015-08-25 10:30:32 +0200

    fix email field

--- admtooCore/models/user.py
+++ admtooCore/models/user.py
@@ -20,7 +20,7 @@ class User (models.Model) :
 	first_name	= models.CharField(max_length=128, null=True, blank=True)
 	last_name   = models.CharField(max_length=128, null=True, blank=True)
 	birthdate   = models.DateField(null=True, blank=True)
-	mail        = models.EmailField(null=True, blank=True)
+	mail        = models.EmailField(max_length=254,null=True, blank=True)
 	manager     = models.ForeignKey('self', null=True, blank=True)
 	userclass   = models.ForeignKey ('UserClass', null=True, blank=True)
 	arrival		= models.DateField(null=True, blank=True)
