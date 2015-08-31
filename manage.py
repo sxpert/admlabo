06791b8314b5f2715af07774cac1f46c52efdcd9
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2015-08-31 14:56:04 +0200

    fix manage

--- manage.py
+++ manage.py
@@ -2,6 +2,11 @@
 import os
 import sys
 
+path = os.path.abspath (os.path.dirname(os.path.dirname(__file__)))
+
+if path not in sys.path :
+	sys.path.append (path)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admtooCore.settings")
 
