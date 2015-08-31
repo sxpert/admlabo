dba3da803c25cc2e0a314a2b32099b1a35b8c1bd
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2015-08-31 13:47:55 +0200

    attempt to fix sys.path

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,18 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.abspath (os.path.dirname(__file__))
+
+print path
+
+if path not in sys.path :
+	sys.path.append (path)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admtooCore.settings")
+
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
