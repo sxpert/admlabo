986a52a6341bfef1ad7471d6e6cb51d81f7545eb
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2015-10-14 04:22:49 +0200

    hide deleted users from user list

--- admtooCore/views/UserList.py
+++ admtooCore/views/UserList.py
@@ -10,7 +10,7 @@ from decorators import *
 
 @admin_login
 def users (request) :
-	users = models.User.objects.all().order_by('last_name')
+	users = models.User.objects.exclude(user_state=models.User.DELETED_USER).order_by('last_name')
 	context = {
 		'users': users,
 	}
